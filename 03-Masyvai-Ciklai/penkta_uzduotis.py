54b0f130962475abc9cfb7d9ad23a1faa5810b63
Mantas Skara <mantas.skara@gmail.com>
2022-05-15 19:16:59 +0300

    Made going trough dict more clear

--- 03-Masyvai-Ciklai/penkta_uzduotis.py
+++ 03-Masyvai-Ciklai/penkta_uzduotis.py
@@ -11,13 +11,13 @@ filtered_surnames = set()
 
 # value for  key, value in my_dict.items() if key[0] == "airport"
 
-for item in salaries.items():
-    name = item[0][0]
-    surname = item[0][1]
-    salary = item[1]
+for key, value in salaries.items():
+    name = key[0]
+    surname = key[1]
+    salary = value
     if len(surname) > 5 and salary > 9000:
         print(f"Employees name is: {name} {surname}, employee earns: {salary}")
         filtered_surnames.add(surname)
-        
+
 for filtered_surname in filtered_surnames:
     print("If you have this surname, you have good salary: " + filtered_surname)
