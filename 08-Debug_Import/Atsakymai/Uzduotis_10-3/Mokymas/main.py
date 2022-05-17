d398c62c820699c580f3f94a2c533268c246c1b9
VakarisZ <vakarisz@yahoo.com>
2022-05-17 20:18:11 +0300

    Importo paskaitos atsakymai + Venv ir .exe

---
+++ 08-Debug_Import/Atsakymai/Uzduotis_10-3/Mokymas/main.py
@@ -0,0 +1,7 @@
+from modules.python_kursas import PythonKursas, Kursas
+
+kursas = Kursas("Fizika", "Domantas", 40)
+python_kursas = PythonKursas("Python", "Donatas", 80)
+
+kursas.destyti()
+python_kursas.destyti()
---
+++ 10-Venv/Uzduotys/hard_weather.py
@@ -0,0 +1,2 @@
+# Parasyti programa, kuri ivedus miesta isvestu to miesto orus
+# Sugeneruoti sitos programos requirements.txt. Ne parasyti, o SUGENERUOTI.
