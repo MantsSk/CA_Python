91d4c3a44799d7408a5ce11efc638d4ab1d3d431
Mantas Skara <mantas.skara@gmail.com>
2022-08-17 21:46:31 +0300

    medziag

---
+++ 51-JSIntro/my_shiny_project/failas.js
@@ -0,0 +1,12 @@
+let x = 50;
+let y = 10;
+
+let suma = x + y;
+let minusas = x - y;
+let dalyba = x / y;
+let daugyba = x * y;
+
+console.log(suma)
+console.log(minusas)
+console.log(dalyba)
+console.log(daugyba)
