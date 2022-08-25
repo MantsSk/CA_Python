0fc424dd026f2cdf6aa7732e2259a0ed05ceebbf
Mantas <mantassk@unity3d.com>
2022-08-25 20:38:02 +0300

    add lesson progress

--- 54-JSDOM/5uzduotis.js
+++ 54-JSDOM/5uzduotis.js
@@ -5,4 +5,20 @@
 // document.body.append(h2);
 
 // 2 
+// var test = document.createElement('section');
+// test.setAttribute('id', 'test');
 
+// var ul = document.createElement('ul');
+// brandai = ["VW", "BMW", "AUDI"]
+
+// document.body.appendChild(test);
+// test.appendChild(ul);
+
+// for (var i = 0; i < brandai.length; i++) {
+
+//   var li = document.createElement('li');
+
+//   ul.appendChild(li);
+//   li.textContent = "Hello"
+
+// }
