b8c5377dd582339e31799825130b8c0982c29f31
Mantas Skara <mantas.skara@gmail.com>
2022-09-08 12:05:11 +0300

    revert changes accidental

--- 62-Flask3/db_init.py
+++ 62-Flask3/db_init.py
@@ -1,23 +1,22 @@
-from app import db, app
+from app import db, Query
 
-with app.app_context():
-    db.create_all() # sukurs mūsų lentelę DB
+db.create_all()  # sukurs mūsų lentelę DB
 
 # Iš karto inicijuosime testams keletą įrašų:
-# jonas = Query('Jonas', 'jonas@mail.com', 'Kažkoks labai rimtas atsiliepimas.')
-# antanas = Query('Antanas', 'antanas@mail.lt', 'Antano nuomonė labai svarbi.')
-# juozas = Query('Juozas', 'juozukas@friends.lt', 'Aš labai piktas, nes blogai.')
-# bronius = Query('Bronius', 'bronka@yahoo.com', 'Aš tai linksmas esu, man patinka.')
+jonas = Query('Jonas', 'jonas@mail.com', 'Kažkoks labai rimtas atsiliepimas.')
+antanas = Query('Antanas', 'antanas@mail.lt', 'Antano nuomonė labai svarbi.')
+juozas = Query('Juozas', 'juozukas@friends.lt', 'Aš labai piktas, nes blogai.')
+bronius = Query('Bronius', 'bronka@yahoo.com', 'Aš tai linksmas esu, man patinka.')
 
-# # Pridėsime šiuos veikėjus į mūsų DB
-# db.session.add_all([jonas, antanas, juozas, bronius])
-# # .commit išsaugo pakeitimus
-# db.session.commit()
+# Pridėsime šiuos veikėjus į mūsų DB
+db.session.add_all([jonas, antanas, juozas, bronius])
+# .commit išsaugo pakeitimus
+db.session.commit()
 
-# print(jonas.id)
-# print(antanas.id)
-# print(bronius.id)
-# print(juozas.id)
+print(jonas.id)
+print(antanas.id)
+print(bronius.id)
+print(juozas.id)
 
 # 1
 # 2
