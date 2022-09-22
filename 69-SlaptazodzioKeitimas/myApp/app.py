ea3e9f687c2f6520b219dbfeaf4653dd245a29de
Mantas <mantassk@unity3d.com>
2022-09-22 09:56:14 +0300

    pasiruosta ketvirtadieniui

--- 69-SlaptazodzioKeitimas/myApp/app.py
+++ 69-SlaptazodzioKeitimas/myApp/app.py
@@ -29,8 +29,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'da
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = "codeacademytest18@gmail.com"
-app.config['MAIL_PASSWORD'] = "password"
+app.config['MAIL_USERNAME'] = "login@gmail.com"
+app.config['MAIL_PASSWORD'] = "psw"
 
 db.init_app(app)
 
