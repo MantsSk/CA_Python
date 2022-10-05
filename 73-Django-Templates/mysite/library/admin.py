412eef86f0f86be2263497158591ac02c17de47e
Mantas Skara <mantas.skara@gmail.com>
2022-10-05 19:26:28 +0300

    push first code

---
+++ 73-Django-Templates/mysite/library/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    readonly_fields = ('id',)
+    can_delete = False
+    extra = 0
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'due_back', 'id', 'status')
+    list_editable = ('due_back', 'status')
+    list_filter = ('status', 'due_back')
+    search_fields = ('id', 'book__title')
+    
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'display_books')
+
+admin.site.register(Book, BookAdmin) 
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
