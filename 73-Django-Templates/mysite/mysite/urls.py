ef54571f207f0b4816e8b08db47437e9861a04a5
Mantas <mantassk@unity3d.com>
2022-10-05 21:40:57 +0300

    paskaitos medziaga

--- 73-Django-Templates/mysite/mysite/urls.py
+++ 73-Django-Templates/mysite/mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('library/', include('library.urls')),
     path('admin/', admin.site.urls),
-]
+    path('', RedirectView.as_view(url='library/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
