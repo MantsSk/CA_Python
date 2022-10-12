bcc410271210f868bdc110ef239720fafa023ebf
Mantas <mantassk@unity3d.com>
2022-10-12 21:37:33 +0300

    paskaita

---
+++ 76-Session-Prisijungimas-Mail/mysite/library/views.py
@@ -0,0 +1,85 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, render
+
+from django.http import HttpResponse
+
+from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponse
+from django.views import generic
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+
+    # Suskaičiuokime keletą pagrindinių objektų
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Laisvos knygos (tos, kurios turi statusą 'g')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # Kiek yra autorių
+    num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
+
+    # perduodame informaciją į šabloną žodyno pavidale:
+    context = {
+        'num_visits': num_visits,
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # renderiname index.html, su duomenimis kintamąjame context
+    return render(request, 'index.html', context=context)
+
+
+def authors(request):
+    paginator = Paginator(Author.objects.all(), 2)
+    page_number = request.GET.get('page')
+    paged_authors = paginator.get_page(page_number)
+    context = {
+        'authors': paged_authors
+    }
+    return render(request, 'authors.html', context=context)
+
+
+def author(request, author_id):
+    single_author = get_object_or_404(Author, pk=author_id)
+
+    context = {
+        'author': single_author
+    }
+    return render(request, 'author.html', context)
+
+
+def search(request):
+    """
+    paprasta paieška. query ima informaciją iš paieškos laukelio,
+    search_results prafiltruoja pagal įvestą tekstą knygų pavadinimus ir aprašymus.
+    Icontains nuo contains skiriasi tuo, kad icontains ignoruoja ar raidės
+    didžiosios/mažosios.
+    """
+    query = request.GET.get('query')
+    search_results = Book.objects.filter(Q(title__icontains=query) | Q(summary__icontains=query))
+    author_results = Author.objects.filter(Q(first_name__icontains=query) | Q(last_name__icontains=query))
+    book_instances = BookInstance.objects.filter(Q(book__author__first_name__icontains=query))
+    return render(request, 'search.html', {'books': search_results,
+                                           'authors': author_results,
+                                           'book_instances': book_instances,
+                                           'query': query})
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+    template_name = "book_list.html"
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = "book_detail.html"
+
