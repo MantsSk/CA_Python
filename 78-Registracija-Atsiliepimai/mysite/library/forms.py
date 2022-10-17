e9dd916408dfe8fdf20bb68c552680b21cc667ba
Mantas <mantassk@unity3d.com>
2022-10-17 21:03:48 +0300

    registracija

---
+++ 78-Registracija-Atsiliepimai/mysite/library/forms.py
@@ -0,0 +1,8 @@
+from .models import BookReview
+from django import forms
+
+class BookReviewForm(forms.ModelForm):
+    class Meta:
+        model = BookReview
+        fields = ('content', 'book', 'reviewer',)
+        widgets = {'book': forms.HiddenInput(), 'reviewer': forms.HiddenInput()}
---
+++ 78-Registracija-Atsiliepimai/mysite/library/views.py
@@ -0,0 +1,153 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponse
+from django.views import generic
+from .models import Book, Author, BookInstance, Genre
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.contrib.auth.forms import User
+from django.views.decorators.csrf import csrf_protect
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, reverse
+from .forms import BookReviewForm
+
+# Importuojame FormMixin, kurį naudosime BookDetailView klasėje
+from django.views.generic.edit import FormMixin
+
+def index(request):
+
+    # Suskaičiuokime keletą pagrindinių objektų
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Laisvos knygos (tos, kurios turi statusą 'g')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # Kiek yra autorių
+    num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
+
+    # perduodame informaciją į šabloną žodyno pavidale:
+    context = {
+        'num_visits': num_visits,
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # renderiname index.html, su duomenimis kintamąjame context
+    return render(request, 'index.html', context=context)
+
+
+def authors(request):
+    paginator = Paginator(Author.objects.all(), 2)
+    page_number = request.GET.get('page')
+    paged_authors = paginator.get_page(page_number)
+    context = {
+        'authors': paged_authors
+    }
+    return render(request, 'authors.html', context=context)
+
+
+def author(request, author_id):
+    single_author = get_object_or_404(Author, pk=author_id)
+
+    context = {
+        'author': single_author
+    }
+    return render(request, 'author.html', context)
+
+
+def search(request):
+    """
+    paprasta paieška. query ima informaciją iš paieškos laukelio,
+    search_results prafiltruoja pagal įvestą tekstą knygų pavadinimus ir aprašymus.
+    Icontains nuo contains skiriasi tuo, kad icontains ignoruoja ar raidės
+    didžiosios/mažosios.
+    """
+    query = request.GET.get('query')
+    search_results = Book.objects.filter(Q(title__icontains=query) | Q(summary__icontains=query))
+    author_results = Author.objects.filter(Q(first_name__icontains=query) | Q(last_name__icontains=query))
+    book_instances = BookInstance.objects.filter(Q(book__author__first_name__icontains=query))
+    return render(request, 'search.html', {'books': search_results,
+                                           'authors': author_results,
+                                           'book_instances': book_instances,
+                                           'query': query})
+
+def register(request):
+    if request.method == "POST":
+        # pasiimame reikšmes iš registracijos formos
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        # tikriname, ar sutampa slaptažodžiai
+        if password == password2:
+            # tikriname, ar neužimtas username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, f'Vartotojo vardas {username} užimtas!')
+                return redirect('register')
+            else:
+                # tikriname, ar nėra tokio pat email
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, f'Vartotojas su el. paštu {email} jau užregistruotas!')
+                    return redirect('register')
+                else:
+                    # jeigu viskas tvarkoje, sukuriame naują vartotoją
+                    User.objects.create_user(username=username, email=email, password=password)
+                    messages.info(request, f'Vartotojas {username} užregistruotas!')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Slaptažodžiai nesutampa!')
+            return redirect('register')
+    return render(request, 'registration/register.html')
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+    template_name = "book_list.html"
+
+
+class BookDetailView(FormMixin, generic.DetailView):
+    model = Book
+    template_name = 'book_detail.html'
+    form_class = BookReviewForm
+
+    class Meta:
+        ordering = ['title']
+
+    # nurodome, kur atsidursime komentaro sėkmės atveju.
+    def get_success_url(self):
+        return reverse('book-detail', kwargs={'pk': self.object.id})
+
+    # standartinis post metodo perrašymas, naudojant FormMixin, galite kopijuoti tiesiai į savo projektą.
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    # štai čia nurodome, kad knyga bus būtent ta, po kuria komentuojame, o vartotojas bus tas, kuris yra prisijungęs.
+    def form_valid(self, form):
+        form.instance.book = self.object
+        form.instance.reviewer = self.request.user
+        form.save()
+        return super(BookDetailView, self).form_valid(form)
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    model = BookInstance
+    template_name = 'user_books.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(reader=self.request.user).filter(
+            status__exact='p').order_by('due_back')
