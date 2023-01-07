f12a13ce32abf958834c257839bb7c9bcbf7e903
Mantas Skara <mantassk@unity3d.com>
2023-01-07 12:54:33 +0200

    updated relationship code a bit

--- 65-Flask-Relationships/many2one-with-delete-and-update/app.py
+++ 65-Flask-Relationships/many2one-with-delete-and-update/app.py
@@ -10,9 +10,11 @@ print(basedir)
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'dfgsfdgsdfgsdfgsdf'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'vaikaitevai.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'vaikaitevai.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 
 class Tevas(db.Model):
     __tablename__ = "tevas"
@@ -29,6 +31,7 @@ class Vaikas(db.Model):
     vardas = db.Column("Vardas", db.String)
     pavarde = db.Column("Pavardė", db.String)
     tevai = db.relationship("Tevas", back_populates="vaikas")
+
 
 @app.route("/")
 def home():
@@ -53,16 +56,17 @@ def children():
     return render_template("vaikai.html", visi_vaikai=visi_vaikai)
 
 
-
 @app.route("/naujas_tevas", methods=["GET", "POST"])
 def new_parent():
     db.create_all()
     forma = forms.TevasForm()
     if forma.validate_on_submit():
-        if forma.vaikas.data: 
-            naujas_tevas = Tevas(vardas=forma.vardas.data, pavarde=forma.pavarde.data, vaikas_id=forma.vaikas.data.id)
+        if forma.vaikas.data:
+            naujas_tevas = Tevas(
+                vardas=forma.vardas.data, pavarde=forma.pavarde.data, vaikas_id=forma.vaikas.data.id)
         else:
-            naujas_tevas = Tevas(vardas=forma.vardas.data, pavarde=forma.pavarde.data)
+            naujas_tevas = Tevas(vardas=forma.vardas.data,
+                                 pavarde=forma.pavarde.data)
         db.session.add(naujas_tevas)
         db.session.commit()
         return redirect(url_for('parents'))
@@ -89,6 +93,7 @@ def update(id):
         return redirect(url_for('parents'))
     return render_template("update.html", form=form, tevas=tevas)
 
+
 @app.route("/naujas_vaikas", methods=["GET", "POST"])
 def new_child():
     db.create_all()
@@ -101,12 +106,26 @@ def new_child():
         return redirect(url_for('children'))
     return render_template("prideti_vaika.html", form=forma)
 
+
 @app.route("/vaikas_delete/<int:id>")
 def vaikas_delete(id):
     uzklausa = Vaikas.query.get(id)
     db.session.delete(uzklausa)
     db.session.commit()
     return redirect(url_for('children'))
+
+
+@app.route("/vaikas_update/<int:id>", methods=['GET', 'POST'])
+def vaikas_update(id):
+    form = forms.VaikasForm()
+    vaikas = Vaikas.query.get(id)
+    if form.validate_on_submit():
+        vaikas.vardas = form.vardas.data
+        vaikas.pavarde = form.pavarde.data
+        db.session.commit()
+        return redirect(url_for('children'))
+    return render_template("update_vaikas.html", form=form, vaikas=vaikas)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
