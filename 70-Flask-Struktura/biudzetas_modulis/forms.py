2a22967faa4a76e402a7425ba1e5d8f5c4f66ab9
Mantas Skara <mantassk@unity3d.com>
2023-01-22 13:21:36 +0200

    pasiruosta

--- 70-Flask-Struktura/biudzetas_modulis/forms.py
+++ 70-Flask-Struktura/biudzetas_modulis/forms.py
@@ -9,18 +9,25 @@ class RegistracijosForma(FlaskForm):
     vardas = StringField('Vardas', [DataRequired()])
     el_pastas = StringField('El. paštas', [DataRequired()])
     slaptazodis = PasswordField('Slaptažodis', [DataRequired()])
-    patvirtintas_slaptazodis = PasswordField("Pakartokite slaptažodį", [EqualTo('slaptazodis', "Slaptažodis turi sutapti.")])
+    patvirtintas_slaptazodis = PasswordField("Pakartokite slaptažodį", [
+                                             EqualTo('slaptazodis', "Slaptažodis turi sutapti.")])
     submit = SubmitField('Prisiregistruoti')
 
     def tikrinti_varda(self, vardas):
-        vartotojas = app.Vartotojas.query.filter_by(vardas=vardas.data).first()
-        if vartotojas:
-            raise ValidationError('Šis vardas panaudotas. Pasirinkite kitą.')
+        with app.app_context():
+            vartotojas = app.Vartotojas.query.filter_by(
+                vardas=vardas.data).first()
+            if vartotojas:
+                raise ValidationError(
+                    'Šis vardas panaudotas. Pasirinkite kitą.')
 
     def tikrinti_pasta(self, el_pastas):
-        vartotojas = app.Vartotojas.query.filter_by(el_pastas=el_pastas.data).first()
-        if vartotojas:
-            raise ValidationError('Šis el. pašto adresas panaudotas. Pasirinkite kitą.')
+        with app.app_context():
+            vartotojas = app.Vartotojas.query.filter_by(
+                el_pastas=el_pastas.data).first()
+            if vartotojas:
+                raise ValidationError(
+                    'Šis el. pašto adresas panaudotas. Pasirinkite kitą.')
 
 
 class PrisijungimoForma(FlaskForm):
@@ -29,28 +36,36 @@ class PrisijungimoForma(FlaskForm):
     prisiminti = BooleanField("Prisiminti mane")
     submit = SubmitField('Prisijungti')
 
+
 class IrasasForm(FlaskForm):
     pajamos = BooleanField('Pajamos')
     suma = FloatField('Suma', [DataRequired()])
     submit = SubmitField('Įvesti')
 
+
 class PaskyrosAtnaujinimoForma(FlaskForm):
     vardas = StringField('Vardas', [DataRequired()])
     el_pastas = StringField('El. paštas', [DataRequired()])
-    nuotrauka = FileField('Atnaujinti profilio nuotrauką', validators=[FileAllowed(['jpg', 'png'])])
+    nuotrauka = FileField('Atnaujinti profilio nuotrauką',
+                          validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Atnaujinti')
 
     def tikrinti_varda(self, vardas):
         if vardas.data != app.current_user.vardas:
-            vartotojas = app.Vartotojas.query.filter_by(vardas=vardas.data).first()
+            vartotojas = app.Vartotojas.query.filter_by(
+                vardas=vardas.data).first()
             if vartotojas:
-                raise ValidationError('Šis vardas panaudotas. Pasirinkite kitą.')
+                raise ValidationError(
+                    'Šis vardas panaudotas. Pasirinkite kitą.')
 
     def tikrinti_pasta(self, el_pastas):
         if el_pastas.data != app.current_user.el_pastas:
-            vartotojas = app.Vartotojas.query.filter_by(el_pastas=el_pastas.data).first()
+            vartotojas = app.Vartotojas.query.filter_by(
+                el_pastas=el_pastas.data).first()
             if vartotojas:
-                raise ValidationError('Šis el. pašto adresas panaudotas. Pasirinkite kitą.')
+                raise ValidationError(
+                    'Šis el. pašto adresas panaudotas. Pasirinkite kitą.')
+
 
 class UzklausosAtnaujinimoForma(FlaskForm):
     el_pastas = StringField('El. paštas', validators=[DataRequired(), Email()])
@@ -59,10 +74,12 @@ class UzklausosAtnaujinimoForma(FlaskForm):
     def validate_email(self, el_pastas):
         user = app.Vartotojas.query.filter_by(el_pastas=el_pastas.data).first()
         if user is None:
-            raise ValidationError('Nėra paskyros, registruotos šiuo el. pašto adresu. Registruokitės.')
+            raise ValidationError(
+                'Nėra paskyros, registruotos šiuo el. pašto adresu. Registruokitės.')
 
 
 class SlaptazodzioAtnaujinimoForma(FlaskForm):
     slaptazodis = PasswordField('Slaptažodis', validators=[DataRequired()])
-    patvirtintas_slaptazodis = PasswordField('Pakartokite slaptažodį', validators=[DataRequired(), EqualTo('slaptazodis')])
+    patvirtintas_slaptazodis = PasswordField('Pakartokite slaptažodį', validators=[
+                                             DataRequired(), EqualTo('slaptazodis')])
     submit = SubmitField('Atnaujinti Slaptažodį')
