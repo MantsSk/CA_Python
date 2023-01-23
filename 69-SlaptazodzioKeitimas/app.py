9922659ab8ce54f5f0518ba0d7bd0e2f3d4130fa
Mantas Skara <mantassk@unity3d.com>
2023-01-23 10:52:59 +0200

    update

--- 69-SlaptazodzioKeitimas/app.py
+++ 69-SlaptazodzioKeitimas/app.py
@@ -78,7 +78,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = "codetest250@gmail.com"
-app.config['MAIL_PASSWORD'] = "qvnobqalcblmfhdi"
+app.config['MAIL_PASSWORD'] = "pakeistas_slaptazodis"
 
 mail = Mail(app)
 
--- ReplacedCode/69-SlaptazodzioKeitimas-old/myApp/password.py
+++ ReplacedCode/69-SlaptazodzioKeitimas-old/myApp/password.py
@@ -1,1 +1,1 @@
-password = "pqopmgzsnainzvgv"
+password = "test"
