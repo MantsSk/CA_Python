9649825677422345b3bcea3f5afac2ec6be73827
Dovydas.p <47259306+DovydasPl@users.noreply.github.com>
2023-01-24 19:57:55 +0200

    Feat/django task (#8)
    * Feat: added autoservice task code
    
    * feat: added code
    
    * feat: added code
    
    Co-authored-by: Dovydas <dovydas@seemsneat.com>

---
+++ 72-Django-models/task/mysite/autoservice/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import CarMake, Car, Order, OrderService, Service
+
+admin.site.register(Car)
+admin.site.register(CarMake)
+admin.site.register(Order)
+admin.site.register(OrderService)
+admin.site.register(Service)
+# Register your models here.
---
+++ 72-Django-models/task/mysite/autoservice/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Automobilio_modelis(models.Model):
+    name = models.CharField(
+        'Title', max_length=200, help_text='Input book\'s genre (for example: detective)')
+
+    def __str__(self):
+        return self.name
+
+
+class CarMake(models.Model):
+    make = models.CharField('Make', max_length=20,
+                            help_text='Car make', null=False)
+    model = models.CharField('Model', max_length=20,
+                             help_text='Car model', null=False)
+
+    def __str__(self):
+        return f'{self.make} {self.model}'
+
+
+class Car(models.Model):
+    license_plate = models.CharField(
+        'Licence plate', max_length=10, help_text='Car\'s licence plate', null=False)
+    car_make = models.ForeignKey(
+        'CarMake', on_delete=models.CASCADE, null=False)
+    vin_code = models.CharField(
+        'Vin code', max_length=20, help_text='Car\'s vin code', null=False)
+    client = models.CharField('Client', max_length=50,
+                              help_text='Client\'s name', null=False)
+
+    def __str__(self):
+        return f'{self.car_make} ({self.license_plate})'
+
+
+class Order(models.Model):
+    date = models.DateField('Date', help_text='Order\'s date', null=False)
+    car = models.ForeignKey('Car', on_delete=models.CASCADE, null=False)
+
+    def __str__(self):
+        return f'{self.car} ({self.date})'
+
+
+class Service(models.Model):
+    title = models.CharField(
+        'Title', help_text='Title of the service', max_length=50, null=False)
+    price = models.FloatField('Price', help_text='The price of the service')
+
+    def __str__(self):
+        return f'{self.title} ({self.price})'
+
+
+class OrderService(models.Model):
+    service = models.ForeignKey(
+        'Service', on_delete=models.CASCADE, null=False)
+    order = models.ForeignKey('Order', on_delete=models.CASCADE, null=False)
+    quantity = models.IntegerField(
+        'Quantity', help_text='Quantity of the service', null=False)
+
+    def __str__(self):
+        return f'{self.service} ({self.order}x{self.quantity})'
